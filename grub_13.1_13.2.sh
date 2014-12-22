2a8b9ff27e8faaf7050edbd1fada354c905c86f2
putztzu <tonysu@su-networking.com>
2014-12-22 12:32:25 -0800

    adding script modifying grub menu for openSUSE 13.1 > 13.2 upgrades

---
+++ grub_13.1_13.2.sh
@@ -0,0 +1,11 @@
+# This script fixes the grub menu when upgrading 13.1 > 13.2
+
+
+# Backing up original
+cp /boot/grub2/grub.cfg /boot/grub2/original_grub_cfg
+# Parse grub info file. Replace 13.1 with 13.2
+sed -i 's/13.1/13.2/g' /etc/default/grub
+# Re-compile grub2
+grub2-mkconfig > /boot/grub2/grub.cfg
+# Done
+echo Done.
