1970434f568bb9723016b10c09f85c502acee087
putztzu <tonysu@su-networking.com>
2015-07-17 09:38:10 -0700

    rm patched* temporarily

--- patched-open-vm-tools.sh
+++
@@ -1,7 +0,0 @@
-zypper in git make gcc kernel-devel wget && \
-git clone https://github.com/rasa/vmware-tools-patches.git && \
-cd vmware-tools-patches && \
-./download-tools.sh && \
-./untar-and-patch.sh && \
-./compile.sh
-
