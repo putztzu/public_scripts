e9ef0caae12760462ad439213c20adb9b6069e37
putztzu <tonysu@su-networking.com>
2015-11-18 11:13:10 -0800

    Script_command example how to download a raw file from github

---
+++ download_raw_file_from_github.sh
@@ -0,0 +1,7 @@
+## User curl command
+## insert raw before the file URL
+
+## curl https://raw.github.com/username/repo/filename
+
+## The following is a working example
+# curl https://raw.github.com/putztzu/public_scripts/blob/master/devstack_on_13.2.sh
